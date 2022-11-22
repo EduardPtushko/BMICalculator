df55bb2af759afdec66be83f9ebe023311ea82fd
Eduard <ptushko@gmail.com>
2022-11-22 11:20:18 +0300

    Initial Commit

---
+++ BMI Calculator/BMI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorApp.swift
+//  BMI Calculator
+//
+//  Created by Eduard on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
